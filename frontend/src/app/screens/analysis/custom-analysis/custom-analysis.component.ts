3e078bc751d7f5596bbd1b41a5713755050cf6f5
Johannes Beiser <beiserjohannes@googlemail.com>
2022-05-16 23:28:52 +0200

    Custom Analysis filter added

--- frontend/src/app/screens/analysis/custom-analysis/custom-analysis.component.ts
+++ frontend/src/app/screens/analysis/custom-analysis/custom-analysis.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category, CategoryService } from 'src/app/services/category/category.service';
+import { Group, GroupsService } from 'src/app/services/groups/groups.service';
+import { map, shareReplay} from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-custom-analysis',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomAnalysisComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private groupsService: GroupsService,
+    private categoryService: CategoryService,
+  ) { }
+
+  public groupsWithSubgroups$: Observable<Group[]>;
+  public groupsWithSubgroups: Group[];
+
+  public categories$: Observable<Category[]>;
+  public categories: Category[];
+
+  selectedGroup: string = "999";
+  selectedGroup$: Subject<string> = new Subject();
+
+  selectedCategory: string = "999";
+  selectedCategory$: Subject<string> = new Subject();
 
   ngOnInit(): void {
+    this.groupsWithSubgroups$ = this.groupsService.getGroups().pipe(
+      map(groups=>{
+        return groups.reduce((acc,cur)=>{
+          acc.push(cur);
+          cur.subgroups.forEach(subgroup=>acc.push(subgroup));
+          return acc;
+        },[{name: "ALLE", id: 999}, {name: "General", id:0}] as Group[])
+      }),
+      shareReplay()
+    );
+
+    this.categories$ = this.categoryService.getCategoriesNew().pipe(
+      map(categories=>{
+        categories.push({name: "ALLE", id: 999} as Category);
+        return categories;
+      })
+    );
+
+    this.groupsWithSubgroups$.subscribe(el=>{
+      this.groupsWithSubgroups = el;
+    });
+
+    this.categories$.subscribe(el=>{
+      this.categories = el;
+    });
+  }
+
+
+  groupChanged(){
+    this.selectedGroup$.next(this.selectedGroup);
+  }
+
+  categoryChanged(){
+    this.selectedGroup$.next(this.selectedCategory);
   }
 
 }
